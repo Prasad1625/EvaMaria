eff9c0162cb344739e95bfa24b00d6fe7f501819
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-08-21 22:16:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = "mongodb+srv://erich:erich@cluster0.6oect.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://erich:erich@cluster0.6oect.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE""
 DATABASE_NAME = "Cluster0"
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_file')
 
